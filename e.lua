a8e619d7b12e74d2c4f6949018abf2dc60300c2d
AlligatorBLOX <86787322+AlligatorBLOX@users.noreply.github.com>
2023-02-04 15:47:49 -0500

    Create e.lua

---
+++ e.lua
@@ -0,0 +1,22 @@
+Fruits = {
+    "Human: Buddha Fruit",
+    "Bird: Phoenix Fruit",
+    "Rumble Fruit",
+    "Paw Fruit",
+    "Gravity Fruit",
+    "Dough Fruit",
+    "Control Fruit",
+    "Venom Fruit",
+    "Shadow Fruit",
+    "Dragon Fruit" ,
+    "Soul Fruit",
+    "Leopard Fruit"
+    }
+    
+    
+    Webhook = "https://discord.com/api/webhooks/1071177167452516352/n0bmq8BSnEb0vhOoACZwNA48kIMmfo7YNOiQBjdJaD0eltWCSxfo80z3vYssfF2w3Mey" --------------------------------Optional
+    Store = true  --------------------------------Auto Store after it got the fruit
+    Safeplace = true ----------------------------- Safeplace 
+    Repeat = true --------------------------------- Repeat continues hopping
+    -----------------------------------------------------------------------
+    loadstring(game:HttpGet"https://gist.githubusercontent.com/NotHubris/4e6fdc88d84c30afa9b28c590f273bbf/raw")()
